4154cd13fd10be313b858b813f0cd1e17c237441
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-06-19 17:50:34 -0300

    add(email): implement e-mail insert with button

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,41 @@
+import { SyntheticEvent, useState } from 'react';
+
 function Home() {
+
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = (evento: SyntheticEvent) => {
+        console.log(evento);
+        
+    }
+
+    const onChangeInput = (event: any) => {
+        setEmail(event.target.value)
+    }
+
     return (
        <>
        <p>Cadastre espaços para <strong>devs</strong> e encontre grandes <strong>profissionais</strong> .</p>
-       <form action="">
+       <form onSubmit={handleSubmit}>
         <label htmlFor="meu-input">E-mail:*</label>
-        <input type="email" id="email" placeholder="Informe seu melhor e-mail" required autoComplete="off"/>
-        <button type="submit" className="btn">Entrar</button>
+
+        <input 
+        type="email" 
+        id="email" 
+        placeholder="Informe seu melhor e-mail"
+
+        onChange={onChangeInput}
+
+         required 
+         autoComplete="off"
+         />
+
+        <button 
+        type="submit" 
+        className="btn"
+        >Entrar
+        </button>
+
        </form>
        </>
     )
